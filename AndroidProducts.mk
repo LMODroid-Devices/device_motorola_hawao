bb292029234740a552aba2c471d43ced6258e560
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:29:37 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hawao.mk
+    $(LOCAL_DIR)/lmodroid_hawao.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_hawao-user \
-    lineage_hawao-userdebug \
-    lineage_hawao-eng
+    lmodroid_hawao-user \
+    lmodroid_hawao-userdebug \
+    lmodroid_hawao-eng
--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 # Permissions
 PRODUCT_COPY_FILES += \
